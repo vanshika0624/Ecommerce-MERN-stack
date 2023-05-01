b40d4c6b8a8e6b85b3188fdbf9e6c4607bb46470
Vanshika Sharma <vsharma0624@gmail.com>
2023-05-01 14:04:19 -0700

    address page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,16 @@
 // import logo from './logo.svg';
 
 import './App.css';
-import DisplayPage from "./component/DisplayPage.js";
-import SignIn from './component/UserPages/signin.js';
-import SignUp from './component/UserPages/signup.js';
+// import DisplayPage from "./component/DisplayPage.js";
+// import SignIn from './component/UserPages/signin.js';
+// import SignUp from './component/UserPages/signup.js';
+import Address from './component/UserPages/address.js';
 function App() {
   return (
     // <SignIn />
     // <DisplayPage />
-    <SignUp />
+    // <SignUp />
+    <Address />
 
   );
 }
