65b46974ca28cfb0bb572a1b8cc059f08630b931
Vanshika Sharma <vsharma0624@gmail.com>
2023-05-10 13:59:02 -0700

    some downloads

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,18 +2,17 @@
 
 import './App.css';
 // import DisplayPage from "./component/DisplayPage.js";
-// import SignIn from './component/UserPages/signin.js';
-import SignUp from './component/UserPages/signup.js';
+import SignIn from './component/UserPages/signin.js';
+// import SignUp from './component/UserPages/signup.js';
 // import SellerSignUp from './component/SellerPages/signup.js'
 // import Address  from './component/address';
 function App() {
   return (
     <div>
       {/* <DisplayPage /> */}
-      {/* <SignIn />
-     */}
-      <SignUp />
-      {/* <SellerSignUp></SellerSignUp>  */}
+      <SignIn />
+      {/* <SignUp /> */}
+      {/* <SellerSignUp></SellerSignUp> */}
       {/* <Address/> */}
     </div>
 
